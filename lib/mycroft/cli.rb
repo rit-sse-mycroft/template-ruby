012a3c8c32ae7ae26efc3bb8299f311df1b86db5
kristenmills <kristen@kristen-mills.com>
2014-01-14 14:28:24 -0500

    Updated the cli

--- lib/mycroft/cli.rb
+++ lib/mycroft/cli.rb
@@ -8,10 +8,14 @@ module Mycroft
     def new(app_name)
       camelcase = app_name.camelize
       underscore = app_name.underscore
+      dashed = app_name.dasherize
       path = "#{Gem.dir}/gems/mycroft-#{Mycroft::VERSION}/lib/mycroft/templates/"
       app_template = File.read("#{path}/app_template")
       app_template.gsub!(/%%APPNAME%%/, camelcase)
+      app_template.gsub!(/%%UNDERSCORE%%/, underscore)
+
       app_manifest = File.read("#{path}/app_manifest")
+      app_manifest.gsub!(/%%DASHED%%/, dashed)
 
       app_file = File.open("./#{underscore}.rb", 'w')
       app_file.puts app_template
