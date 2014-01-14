f6bc0af11e8213b910ada5ea70beaf2995b146d6
kristenmills <kristen@kristen-mills.com>
2014-01-14 11:45:24 -0500

    added the connect to mycroft method

--- lib/mycroft_ruby/lib.rb
+++ lib/mycroft_ruby/lib.rb
@@ -1,6 +1,10 @@
 require 'json'
+require 'socket'
+require 'openssl'
 
 module MycroftRuby
+  MYCROFT_PORT = 1847
+
   # Parses a message
   def parse_message(msg)
     msg = msg.to_s
@@ -17,5 +21,30 @@ module MycroftRuby
       data = JSON.parse(msg_split[3])
     end
     {type: type, data: data}
+  end
+
+  # Connects to mycroft
+  def connect_to_mycroft(key='', cert='')
+    client = nil
+    if ARGV.length == 1 and ARGV[0] == '--no-tls'
+      puts 'Not Using TLS'
+      begin
+        client = TCPSocket.open('localhost', MYCROFT_PORT)
+      rescue
+        puts "There was an error establishing connection"
+      end
+    else
+      console.log('Using TLS')
+      ssl_context = OpenSSL::SSL::SSLContext.new
+      ssl_context.cert = OpenSSL::X509::Certificate.new(File.open(cert))
+      ssl_context.key = OpenSSL::PKey::RSA.new(File.open(key))
+      ssl_socket = OpenSSL::SSL::SSLSocket.new(socket, ssl_context)
+      begin
+        ssl_socket.connect
+      rescue
+        puts "There was an error in establishing TLS connection"
+      end
+    end
+    client
   end
 end
