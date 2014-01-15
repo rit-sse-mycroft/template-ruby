ddadb2b8587a5e89f86eb9b0af737d4ff491567b
kristenmills <kristen@kristen-mills.com>
2014-01-15 10:39:46 -0500

    Added the client class

---
+++ lib/mycroft/client.rb
@@ -0,0 +1,17 @@
+require 'eventmachine'
+
+module Mycroft
+  class Client < EventMachine::Connection
+    def post_init
+
+    end
+
+    def recieve_data
+
+    end
+
+    def unbind
+
+    end
+  end
+end
